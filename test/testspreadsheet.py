400e1d66044cae1e8d518da8e9d1b6b7b7a6618f
Hyrkas <Miika.Hyrkas@student.oulu.fi>
2024-10-29 17:22:15 +0200

    US1 - Test 1

--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -4,5 +4,17 @@ from spreadsheet import SpreadSheet
 
 class TestSpreadSheet(TestCase):
 
-    def test_something(self):
-        pass
+    def test_evaluate_unsigned_integer(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "1")
+        self.assertEqual(1, spreadsheet.evaluate("A1"))
+
+    def test_evaluate_signed_integer(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "-1")
+        self.assertEqual(-1, spreadsheet.evaluate("A1"))
+
+    def test_evaluate_invalid_input(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "1.5")
+        self.assertEqual("#Error", spreadsheet.evaluate("A1"))
