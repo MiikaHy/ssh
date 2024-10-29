b3465d62a3f90cc29e1c4bdcb0e66bb04b63c7a7
Hyrkas <Miika.Hyrkas@student.oulu.fi>
2024-10-29 17:29:05 +0200

    US2 - Test 2

--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -23,3 +23,8 @@ class TestSpreadSheet(TestCase):
         spreadsheet = SpreadSheet()
         spreadsheet.set("A1", "'Apple'")
         self.assertEqual("Apple", spreadsheet.evaluate("A1"))
+
+    def test_evaluate_no_single_quotes(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "Apple")
+        self.assertEqual("#Error", spreadsheet.evaluate("A1"))
